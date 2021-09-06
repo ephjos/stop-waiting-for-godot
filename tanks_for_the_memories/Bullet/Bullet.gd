d8c0da17b9b760ba86a524699a37d7235f656556
Joseph Hines <jthines3@gmail.com>
2021-09-06 14:29:51 -0400

    Added basic obstacles

--- tanks_for_the_memories/Bullet/Bullet.gd
+++ tanks_for_the_memories/Bullet/Bullet.gd
@@ -11,6 +11,9 @@ func _physics_process(delta):
 func _on_Bullet_body_entered(body):
 	if body.is_in_group("mobs") or body.is_in_group("player"):
 		body.hit()
+		
+	if body.is_in_group("low_obstacle"):
+		return
 	queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
